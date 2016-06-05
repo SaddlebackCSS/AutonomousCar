ce57df405407350a939b3ec92580770060ba5de8
root <root@beaglebone.localdomain>
2016-06-05 19:38:34 +0000

    Fix formatting

--- src/Motor.cpp
+++ src/Motor.cpp
@@ -8,51 +8,57 @@
 #include "../include/util.h"
 #include <cstdlib>
 #include <iostream>
-namespace exploringBB{
-
-Motor::Motor():PWM("car_motor.17"),esc_switch(60), PWM_MAX(2000000), PWM_MIN(1000000) {
-    std::cout << "motor constructor" << std::endl;
-        init();
-}
-
-void Motor::init(){
-    std::cout << "running init function" << std::endl;
-    unsigned int CENTER = 1500000;
-    setDutyCycle(CENTER);
-    esc_switch.setDirection(OUTPUT);
-    esc_switch.setValue(HIGH);
-    usleep(2000000);
-    std::cout << "done waiting! :D" << std::endl;
-}
-
-void Motor::move(int percent)
+namespace exploringBB
 {
-	// If the value is invalid DO NOTHING
-	if( percent < -100 || percent > 100){ return; }
-	
-	unsigned int CENTER = 1500000;
-    unsigned int SCALE  = 500000;
-	unsigned int duty = (unsigned int)(CENTER + (SCALE * percent / 100.0));
-	
-	if( percent < 0 && getDutyCycle() >= CENTER ) {
-		std::cout << "Going reverse from a forward speed!" << std::endl;
-		// double tap to go backwards from a >= 0
-		setDutyCycle(duty);
-		setDutyCycle(CENTER);
-		setDutyCycle(duty);
 
-        std::cout << "duty : " << duty << std::endl;
-        setDutyCycle(duty);
-	}
-	else {
-		std::cout << "duty : " << duty << std::endl;
-        setDutyCycle(duty);
-	}
-}
+    Motor::Motor():PWM("car_motor.17"),esc_switch(60), PWM_MAX(2000000), PWM_MIN(1000000) 
+    {
+        std::cout << "motor constructor" << std::endl;
+        init();
+    }
 
-Motor::~Motor() {
-    esc_switch.setValue(LOW);
-}
+    void Motor::init()
+    {
+        std::cout << "running init function" << std::endl;
+        unsigned int CENTER = 1500000;
+        setDutyCycle(CENTER);
+        esc_switch.setDirection(OUTPUT);
+        esc_switch.setValue(HIGH);
+        usleep(2000000);
+        std::cout << "done waiting! :D" << std::endl;
+    }
+
+    void Motor::move(int percent)
+    {
+        // If the value is invalid DO NOTHING
+        if( percent < -100 || percent > 100){ return; }
+        
+        unsigned int CENTER = 1500000;
+        unsigned int SCALE  = 500000;
+        unsigned int duty = (unsigned int)(CENTER + (SCALE * percent / 100.0));
+        
+        if( percent < 0 && getDutyCycle() >= CENTER ) 
+        {
+            std::cout << "Going reverse from a forward speed!" << std::endl;
+            // double tap to go backwards from a >= 0
+            setDutyCycle(duty);
+            setDutyCycle(CENTER);
+            setDutyCycle(duty);
+
+            std::cout << "duty : " << duty << std::endl;
+            setDutyCycle(duty);
+        }
+        else 
+        {
+            std::cout << "duty : " << duty << std::endl;
+            setDutyCycle(duty);
+        }
+    }
+
+    Motor::~Motor() 
+    {
+        esc_switch.setValue(LOW);
+    }
 
 
 } /* namespace exploringBB */
